68cc1078fea5aeb3a7c9b2b49e4608ce2b3c7631
nJavid11 <tu201803071@code.edu.az>
2021-10-28 07:32:14 +0400

    last

---
+++ LastTask/LastTask/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LastTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a string: ");
+            string s = Console.ReadLine();
+            Console.WriteLine("Numbers of spaces: " + task11(s));
+        }
+
+        #region task11
+        public static int task11(string s)
+        {
+            int count = 0;
+            foreach (char ch in s)
+            {
+                if (ch.Equals(' '))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        #endregion
+    }
+}
